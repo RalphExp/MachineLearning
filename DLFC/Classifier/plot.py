15df8368186337ce3e0e103066971f1e408f2947
Ralph <ralphxwz@gmail.com>
2025-06-01 15:26:10 +0900

    1

---
+++ DLFC/Classifier/plot.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('o', 's', '^' , 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    # X[:, 0] => get the 0th feature column
+    # X[:, 1] => get the 1st feature column
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
+                           np.arange(x2_min, x2_max, resolution))
+    
+    # flatten the grid matrix
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        # this is an extractor, which extract those elements
+        # which mask is True
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+        alpha=0.8,
+        c=colors[idx],
+        marker=markers[idx],
+        label=f'Class {cl}', edgecolor='black')
+
+if __name__ == '__main__':
+    pass
