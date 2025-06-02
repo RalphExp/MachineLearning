282f19e092d6b3cea2326f477c6e7aa2adc7b823
Ralph <ralphxwz@gmail.com>
2025-06-02 13:36:04 +0900

    1

--- DLFC/dlfc/plot.py
+++ DLFC/dlfc/plot.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from matplotlib.colors import ListedColormap
 
-def plot_decision_regions(X, y, classifier, resolution=0.02):
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     # setup marker generator and color map
     markers = ('o', 's', '^' , 'v', '<')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -29,9 +29,26 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     for idx, cl in enumerate(np.unique(y)):
         # this is an extractor, which extract those elements
         # which mask is True
-        plt.scatter(x=X[y == cl, 0], 
-                    y=X[y == cl, 1],
-        alpha=0.8,
-        c=colors[idx],
-        marker=markers[idx],
-        label=f'Class {cl}', edgecolor='black')
+        plt.scatter(
+            x=X[y == cl, 0], 
+            y=X[y == cl, 1],
+            alpha=0.8,
+            c=colors[idx],
+            marker=markers[idx],
+            label=f'Class {cl}', edgecolor='black'
+        )
+
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='none',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='Test set') 
