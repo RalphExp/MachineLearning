eeb9f84509a6a362ba74075550baa161a5a06049
Ralph <ralphxwz@gmail.com>
2025-07-21 21:27:49 +0900

    add LDA

--- DLFC/dlfc/plot.py
+++ DLFC/dlfc/plot.py
@@ -41,7 +41,8 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     # highlight test examples
     if test_idx:
         # plot all examples
-        X_test, y_test = X[test_idx, :], y[test_idx]
+        # X_test, y_test = X[test_idx, :], y[test_idx]
+        X_test = X[test_idx, :]
 
         plt.scatter(X_test[:, 0],
                     X_test[:, 1],
